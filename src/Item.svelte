84605b523cf59124e8e625c60bced9d096ffebc6
Lucas Genzelis <lucas.genzelis@gmail.com>
2021-04-26 12:23:03 -0300

    improve li style

--- src/Item.svelte
+++ src/Item.svelte
@@ -3,9 +3,26 @@
 </script>
 
 <style>
+  li {
+    list-style-type: none;
+    font-weight: bold;
+  }
+
+  li::before {
+    content: '✅';
+    margin-right: 5px;
+  }
+
   .crossed {
     text-decoration: line-through;
   }
+
+  li.crossed {
+    list-style-type: none;
+    font-weight: normal;
+    filter: grayscale(0.6);
+  }
+
 </style>
 
 <li on:click class:crossed={todo.done}>{todo.text}</li>
