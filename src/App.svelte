9150b948f184ccdbd345c7566865789865a6b745
Lucas Genzelis <lucas.genzelis@gmail.com>
2021-06-10 10:34:34 -0300

    tabs to space

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,38 @@
 <script lang="ts">
-	import Item from './Item.svelte'
-	export let initialTodo: string;
-	let todos = [];
-	let newTodo = initialTodo;
+  import Item from './Item.svelte'
+  export let initialTodo: string;
+  let todos = [];
+  let newTodo = initialTodo;
 
-	function addTodo() {
-		todos = [...todos, { text: newTodo, done: false }]
-		newTodo = '';
-	}
+  function addTodo() {
+    todos = [...todos, { text: newTodo, done: false }]
+    newTodo = '';
+  }
 
-	function toggleTodo(index) {
-		const { text, done } = todos[index];
-		const newTodos = todos.slice();
-		newTodos[index] = { text, done: !done };
-		todos = newTodos;
-	}
+  function toggleTodo(index) {
+    const { text, done } = todos[index];
+    const newTodos = todos.slice();
+    newTodos[index] = { text, done: !done };
+    todos = newTodos;
+  }
 
-	function clearDoneTodos() {
-		todos = todos.filter(({ done }) => !done)
-	}
+  function clearDoneTodos() {
+    todos = todos.filter(({ done }) => !done)
+  }
 
-	$: isAnyTodoDone = todos.some(({done}) => done)
+  $: isAnyTodoDone = todos.some(({done}) => done)
 
 </script>
 
 <main>
-	<form on:submit|preventDefault={addTodo}>
-		<input bind:value={newTodo} />
-		<button disabled={!newTodo.trim().length}>Add ToDo</button>
-	</form>
-	<ul>
-		{#each todos as todo, idx}
-			<Item todo={todo} on:click={() => toggleTodo(idx)} />
-		{/each}
-	</ul>
-	<button on:click={clearDoneTodos} disabled={!isAnyTodoDone}>Clear completed tasks</button>
+  <form on:submit|preventDefault={addTodo}>
+    <input bind:value={newTodo} />
+    <button disabled={!newTodo.trim().length}>Add ToDo</button>
+  </form>
+  <ul>
+    {#each todos as todo, idx}
+      <Item todo={todo} on:click={() => toggleTodo(idx)} />
+    {/each}
+  </ul>
+  <button on:click={clearDoneTodos} disabled={!isAnyTodoDone}>Clear completed tasks</button>
 </main>
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		initialTodo: 'say hello world'
-	}
+  target: document.body,
+  props: {
+    initialTodo: 'say hello world'
+  }
 });
 
 export default app;
